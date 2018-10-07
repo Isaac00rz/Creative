c582ee5c0fd38ec33dae03f86f41167fa645bf59
angelhr <angelhr1995@hotmail.com>
2018-10-07 12:27:37 -0500

    Menu terminado
    Menu principal de la página terminado

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(main);
  
 var contador=0;
@@ -29,35 +28,3 @@ function main(){
 
 };
 
-=======
-$(document).ready(main);
- 
-var contador=0;
-
-function main(){
-	$('.menu_bar').click(function(){
-
-		if(contador == 1){
-			$('nav').animate({
-				left: '-100%'
-			});
-			contador = 0;
-			
-		} else {
-			contador = 1;
-			$('nav').show(500);
-			$('nav').animate({
-				left: '0'
-			});
-
-		}
-		  $(".menu_bar span").toggle();
-	});
-	
-	$('.submenu').click(function(){
-		$(this).children('.item').slideToggle();
-	});
-
-};
-
->>>>>>> f2478f6f0ec76acba79392add520961683ed4389
