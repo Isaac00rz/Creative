bd1c138a6bcf208110495be63279a54657c494fb
Isaac00rz <Isaac_marqz@outlook.com>
2018-10-18 20:54:27 -0500

    Adelanto de scrip

--- public/Scrip.sql
+++ public/Scrip.sql
@@ -186,3 +186,104 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+create table ComprasImpresoras(
+id_compra integer not null auto_increment,
+fecha datetime default NOW(),
+precio double,
+IVA integer, 
+id  int(10) unsigned NOT NULL,
+id_provedor integer not null,
+PRIMARY KEY (id_compra,id_provedor, id),
+INDEX fk_usuario_Com_Impr (id ASC),
+CONSTRAINT fk_usuario_ComprasImpreso_F
+FOREIGN KEY (id)
+REFERENCES Creative.users (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION,
+CONSTRAINT fk_provedor_ComImpre_F
+FOREIGN KEY (id_provedor)
+REFERENCES Creative.Provedores (id_provedor)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+create table ModeloCompra(
+id_compra integer not null,
+modelo varchar(20) not null,
+PRIMARY KEY (id_compra),
+INDEX fk_ModeloCompra_I (id_compra ASC),
+CONSTRAINT fk_compra_F
+FOREIGN KEY (id_compra)
+REFERENCES Creative.ComprasImpresoras (id_compra)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+create table ComprasConsumibles(
+id_compraC integer not null auto_increment,
+fecha datetime default NOW(),
+precio double,
+IVA integer, 
+id  int(10) unsigned NOT NULL,
+id_provedor integer not null,
+PRIMARY KEY (id_compraC,id_provedor, id),
+INDEX fk_usuario_ComC_Impr (id ASC),
+CONSTRAINT fk_usuario_ComprasCon_F
+FOREIGN KEY (id)
+REFERENCES Creative.users (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION,
+CONSTRAINT fk_provedor_CompraConsu_F
+FOREIGN KEY (id_provedor)
+REFERENCES Creative.Provedores (id_provedor)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+create table ModeloConsumible(
+id_compraC integer not null,
+nombre varchar(25) not null,
+PRIMARY KEY (id_compraC),
+INDEX fk_ModeloConsumi_I (id_compraC ASC),
+CONSTRAINT fk_compraConsu_F
+FOREIGN KEY (id_compraC)
+REFERENCES Creative.ComprasConsumibles (id_compraC)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+create table Ventas(
+id_venta integer not null auto_increment,
+descripcion varchar(150) not null,
+FechaCompra datetime default NOW(),
+IVA integer not null,
+descuentoP integer,
+descuentoE integer,
+Activo boolean default true,
+RFC_cliente integer not null,
+id  int(10) unsigned NOT NULL,
+id_Sucursal integer not null,
+PRIMARY KEY (id_venta,id_Sucursal, id,RFC_cliente),
+INDEX fk_usuario_Ventas_I (id ASC),
+CONSTRAINT fk_usuario_Venta_F
+FOREIGN KEY (id)
+REFERENCES Creative.users (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION,
+CONSTRAINT fk_Sucursal_Venta_F
+FOREIGN KEY (id_Sucursal)
+REFERENCES Creative.Sucursal (id_Sucursal)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION,
+CONSTRAINT fk_Cliente_Venta_F
+FOREIGN KEY (RFC_cliente)
+REFERENCES Creative.Clientes (RFC)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
