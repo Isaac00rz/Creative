91908039d313838b88a375947d6e0e7170ddd579
Isaac00rz <Isaac_marqz@outlook.com>
2018-11-16 21:04:36 -0600

    consutlas de prueba

--- public/consultas de prueba.sql
+++ public/consultas de prueba.sql
@@ -10,6 +10,9 @@ clientes;
 select * from sucursal;
 
 select * from users;
+SET FOREIGN_KEY_CHECKS=0;
+truncate table users;
+SET FOREIGN_KEY_CHECKS=1;
 insert into users(id,name,email,password) values(null,'Admin','admin@hotmail.com','123123');
 
 insert into sucursal values(null,'Leon I','Lomas #4445','Los altos',3554,4772596655,'creative@hotmail.com',NOW(),true,1);
@@ -17,3 +20,10 @@ insert into sucursal values(null,'Leon I','Lomas #4445','Los altos',3554,4772596
 insert into clientes values(374414,'Roberto','Rodriguez','Martinez','Valle de los tacos $425','Valle Alca',37544,4771234745,4442255,'Sapal@sapal.com',NOW(),true,1,1);
 
 update clientes set Activo = 1 where rfc = 374414;
+
+alter table users add (
+username varchar(15) not null
+);
+
+select * from roles;
+insert into roles values (1,'Administrador',0);
